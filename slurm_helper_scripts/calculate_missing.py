ef28e95a474d49487fb92f82d4b3969b847df177
Daneil Buchan <clone@myself.com>
2017-12-08 11:31:52 +0000

    calc missing add

--- slurm_helper_scripts/calculate_missing.py
+++ slurm_helper_scripts/calculate_missing.py
@@ -5,5 +5,5 @@ import re
 pdb_str = "(.+)_\d+_\d+\.pdb"
 pdb_re = re.compile(pdb_str)
 for path in glob.glob(sys.argv[1]+"*_*_*.pdb"):
-    if re.match(path):
+    if pdb_re.search(path):
         print(path)
