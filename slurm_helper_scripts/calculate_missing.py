0618ed46627bb45d98cc1df2e76c5e2dca50b476
Daneil Buchan <clone@myself.com>
2017-12-08 11:51:30 +0000

    calc missing add

--- slurm_helper_scripts/calculate_missing.py
+++ slurm_helper_scripts/calculate_missing.py
@@ -4,7 +4,13 @@ import re
 
 pdb_str = ".+/(.+?)_\d+_\d+\.pdb"
 pdb_re = re.compile(pdb_str)
+complete_genes = []
 for path in glob.glob(sys.argv[1]+"*_*_*.pdb"):
     m = pdb_re.search(path)
     if m:
         print(m.group(1))
+
+
+for path in glob.glob(sys.argv[2]):
+    for fasta in glob.glob(path):
+        print(fasta)
