c9dc655d34ae71b94053c5281543ab503a374c81
Daneil Buchan <clone@myself.com>
2017-12-08 12:00:31 +0000

    calc missing add

--- slurm_helper_scripts/calculate_missing.py
+++ slurm_helper_scripts/calculate_missing.py
@@ -8,9 +8,12 @@ complete_genes = []
 for path in glob.glob(sys.argv[1]+"*_*_*.pdb"):
     m = pdb_re.search(path)
     if m:
-        print(m.group(1))
+        complete_genes.append(m.group(1))
 
-
+fasta_str = ".+/(.+?).fasta"
+fasta_re = re.compile(fasta_str)
 for path in glob.glob(sys.argv[2]+"/*"):
     for fasta in glob.glob(path+"/*"):
-        print(fasta)
+        m = fasta.re.search(fasta)
+        if m:
+            print(m.group(1))
