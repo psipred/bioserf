fe90c79b7d33ed468b9069f5ae245d97cd8a5b83
Daneil Buchan <clone@myself.com>
2017-12-18 15:44:43 +0000

    adding tdb read

--- bin/process_cath_data.py
+++ bin/process_cath_data.py
@@ -110,6 +110,7 @@ domall_list = sys.argv[2]
 tdb_list = sys.argv[3]
 
 domall = read_domall(domall_list)
-#print(domall)
-domall = read_tdb(domall, tdb_list)
+print(domall)
+
+#domall = read_tdb(domall, tdb_list)
 read_domain_list(domall, domain_list)
