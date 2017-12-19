4610d9411ee7f62952a5ad5b8a00fc320a64cb11
Daneil Buchan <clone@myself.com>
2017-12-19 11:00:23 +0000

    adding tdb read

--- bin/process_cath_data.py
+++ bin/process_cath_data.py
@@ -95,10 +95,9 @@ def read_tdb(domall, tdb_list):
         chainid = tdb[-11:-6]
         domain_number = int(tdb[-6:-4])
         domain_len = file_len(tdb)
-        print(chainid)
         if domain_number == 0:
             domain_number == 1
-        # print(domid, chainid, domain_number)
+        print(domid, chainid, domain_number)
         if chainid in domall:
             if domain_number in domall[chainid]:
                 pass
