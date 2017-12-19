31fa034abc71b36fea9f284f4b4e3f98d8943271
Daneil Buchan <clone@myself.com>
2017-12-19 14:10:50 +0000

    adding tdb read

--- bin/process_cath_data.py
+++ bin/process_cath_data.py
@@ -101,14 +101,11 @@ def read_tdb(domall, tdb_list):
         if domain_number == 0:
             domain_number = 1
         if chainid in domall:
-            if domain_number in domall[chainid]:
-                pass
-            else:
-                if len(domall[chainid][domain_number]) > 1:
-                    domall[chainid][domain_number] = {}
-                    domall[chainid][domain_number][1]['start'] = '-'
-                    domall[chainid][domain_number][1]['stop'] = '-'
-                    domall[chainid][domain_number][1]['length'] = domain_len
+            if len(domall[chainid][domain_number]) > 1:
+                domall[chainid][domain_number] = {}
+                domall[chainid][domain_number][1]['start'] = '-'
+                domall[chainid][domain_number][1]['stop'] = '-'
+                domall[chainid][domain_number][1]['length'] = domain_len
         # break
     return(domall)
 
