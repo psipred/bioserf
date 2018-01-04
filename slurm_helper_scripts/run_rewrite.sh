4661a5f3155ede83ad4e3f50439a96f5437d704f
Daniel Buchan <clone@myself.com>
2018-01-04 13:56:30 +0000

    make rewrite work

--- slurm_helper_scripts/run_rewrite.sh
+++ slurm_helper_scripts/run_rewrite.sh
@@ -8,9 +8,6 @@
 #SBATCH --time=6:00:00
 #
 
-export PYTHONPATH=$HOME/Applications/modeller9.19/modlib/:$HOME/Applications/modeller9.19/lib/x86_64-intel8/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Application/modeller9.19/lib/x86_64-intel8/
 cd $HOME/working/genome3d/parse_cath_domth_output/
-for i in `ls *.py | sed -e 's/\.py//'`; do echo $i; $HOME/Applications/modeller9.19/bin/mod9.19 $i.py; done;
-for i in `ls *.ali | sed -e 's/\.ali//'`; do echo $i; $HOME/Applications/bioserf/bin/rewrite_modeller.pl ./ B0R5N0.mod_lookups B0R5N0.blastaligns B0R5N0.pdomaligns ../example/B0R5N0.fasta $i.ali ../bin/reformat.pl; done;`
-sp_P05052_APPY_ECOLI
+for i in `ls *.ali | sed -e 's/\.ali//'`; do echo $i; j=`echo $i | cut -d. -f1`; $HOME/Applications/bioserf/bin/rewrite_modeller.pl $HOME/working/genome3d/parse_cath_domth_ouput/ $HOME/working/genome3d/parse_cath_domth_ouput/$j.mod_lookups $HOME/working/genome3d/parse_cath_domth_ouput/$j.blastaligns $HOME/working/genome3d/parse_cath_domth_ouput/$j.pdomaligns $HOME/working/genome3d/Genome3D.2017-09-05/ecoli_fasta/all/$j.fasta $i.ali $HOME/Applictions/bioserf/bin/reformat.pl; done;
+#for i in `ls *.ali | sed -e 's/\.ali//'`; do echo $i; j=`echo $i | cut -d. -f1`; echo $j; done;
