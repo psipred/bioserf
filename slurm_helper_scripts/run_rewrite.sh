7667b6c44edc37d2774b59df9c565517c065acb0
Daniel Buchan <clone@myself.com>
2018-01-04 14:43:28 +0000

    make rewrite work

--- slurm_helper_scripts/run_rewrite.sh
+++ slurm_helper_scripts/run_rewrite.sh
@@ -9,5 +9,5 @@
 #
 
 cd $HOME/working/genome3d/parse_cath_domth_output/
-for i in `ls *.ali | sed -e 's/\.ali//'`; do echo $i; j=`echo $i | cut -d. -f1`; $HOME/Applications/bioserf/bin/rewrite_modeller.pl $HOME/working/genome3d/parse_cath_domth_ouput/ $HOME/working/genome3d/parse_cath_domth_ouput/$j.mod_lookups $HOME/working/genome3d/parse_cath_domth_ouput/$j.blastaligns $HOME/working/genome3d/parse_cath_domth_ouput/$j.pdomaligns $HOME/working/genome3d/Genome3D.2017-09-05/ecoli_fasta/all/$j.fasta $i.ali $HOME/Applictions/bioserf/bin/reformat.pl; done;
+for i in `ls *.ali | sed -e 's/\.ali//'`; do echo $i; j=`echo $i | cut -d. -f1`; echo $HOME/Applications/bioserf/bin/rewrite_modeller.pl $HOME/working/genome3d/parse_cath_domth_ouput/ $HOME/working/genome3d/parse_cath_domth_ouput/$j.mod_lookups $HOME/working/genome3d/parse_cath_domth_ouput/$j.blastaligns $HOME/working/genome3d/parse_cath_domth_ouput/$j.pdomaligns $HOME/working/genome3d/Genome3D.2017-09-05/ecoli_fasta/all/$j.fasta $i.ali $HOME/Applictions/bioserf/bin/reformat.pl; done;
 #for i in `ls *.ali | sed -e 's/\.ali//'`; do echo $i; j=`echo $i | cut -d. -f1`; echo $j; done;
