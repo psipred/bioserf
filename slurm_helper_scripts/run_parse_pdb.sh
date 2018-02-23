d20a326cce7e3800259fcc97835a002116ae9a74
Daneil Buchan <clone@myself.com>
2018-02-23 15:45:36 +0000

    parse debug

--- slurm_helper_scripts/run_parse_pdb.sh
+++ slurm_helper_scripts/run_parse_pdb.sh
@@ -11,7 +11,7 @@
 
 FILES=($HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/$1/*)
 out_name=${FILES[$SLURM_ARRAY_TASK_ID-1]}
-out_name=${out_name:68:-5}
+out_name=${out_name:70:-5}
 pdb_out_name=$out_name'domserf.pdb.bls'
 echo $out_name
 
