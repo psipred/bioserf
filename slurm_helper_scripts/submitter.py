ec767649641649ab40202bdca773665fc89a203d
Daneil Buchan <clone@myself.com>
2018-02-27 10:56:56 +0000

    add some submitter notes

--- slurm_helper_scripts/submitter.py
+++ slurm_helper_scripts/submitter.py
@@ -3,6 +3,19 @@ import glob
 import time
 import sys
 
+#
+# usage:
+#
+# python submitter [path to batch directories] [path to slurm bash script]
+
+#
+# SLURM script must makre use of something like:
+#
+#  #SBATCH --array=1-1000
+
+#  FILES=($HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/$1/*)
+#  out_name=${FILES[$SLURM_ARRAY_TASK_ID-1]}
+
 path = sys.argv[1]
 command = sys.argv[2]
 
