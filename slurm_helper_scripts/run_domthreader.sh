84939cbedbe360289f4bf913fccb361c59e53b04
Daneil Buchan <clone@myself.com>
2018-02-28 12:05:06 +0000

    better file length handling for domthreader

--- slurm_helper_scripts/run_domthreader.sh
+++ slurm_helper_scripts/run_domthreader.sh
@@ -9,7 +9,9 @@
 #
 #SBATCH --array=1-1000
 
+path=$HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/$1/
+size=${#path}
 FILES=($HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/$1/*)
 out_name=${FILES[$SLURM_ARRAY_TASK_ID-1]}
-out_name=${out_name:70:-6}
+out_name=${out_name:$size:-6}
 $HOME/Applications/pGenTHREADER/GenThreader.sh -i ${FILES[$SLURM_ARRAY_TASK_ID-1]} -j $HOME/working/genome3d/domthreader_output/$out_name -d
