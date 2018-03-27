24b60495254b23502f8689465ec42fe8ff91dbb3
Daneil Buchan <clone@myself.com>
2018-03-27 16:57:14 +0100

    checking_outputs stuff

--- slurm_helper_scripts/check_outputs.py
+++ slurm_helper_scripts/check_outputs.py
@@ -2,22 +2,28 @@
 # have been produced
 
 import glob
+from sets import Set
 
 # get a list of all the targets
 targets = '/home/camp/buchand/working/genome3d/Genome3D.2017-09-05/all_fasta/'
-target_list = {}
+target_list = []
 for target_dir in glob.glob(targets+"*"):
     for target in glob.glob(target_dir+"/*"):
         prot_id = target[target.rfind("/")+1:-6]
         # print(target)
         # print(prot_id)
-        target_list[prot_id] = 0
+        target_list.append(prot_id)
 
 # print(target_list)
 
 results = '/home/camp/buchand/working/genome3d/domthreader_output/'
-result_list = {}
+result_list = []
 for dom_file in glob.glob(results+"*.presults"):
     print(dom_file)
     dom_id = dom_file[dom_file.rfind("/")+1:-14]
-    print(dom_id)
+    result_list.append(dom_id)
+
+
+target_set = Set(target_list)
+result_set = Set(result_list)
+missing_set = target.difference(result_set)
