0d1f28d43ea78ad28d0d7bad0f0c9e826ecf0fcd
Daneil Buchan <clone@myself.com>
2018-03-27 15:52:38 +0100

    checking_outputs stuff

--- slurm_helper_scripts/check_outputs.py
+++ slurm_helper_scripts/check_outputs.py
@@ -8,6 +8,6 @@ targets = '/home/camp/buchand/working/genome3d/Genome3D.2017-09-05/all_fasta/'
 target_list = []
 for target_dir in glob.glob(targets+"*"):
     for target in glob.glob(target_dir+"/*"):
-        prot_id = target[target.rfind("/"):-5]
+        prot_id = target[target.rfind("/")+1:-6]
         print(target)
         print(prot_id)
