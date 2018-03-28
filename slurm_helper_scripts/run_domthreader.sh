4b7830e067fa34992e31971347948fc86fb98c2e
Daneil Buchan <clone@myself.com>
2018-03-28 12:04:30 +0100

    test change

--- slurm_helper_scripts/run_domthreader.sh
+++ slurm_helper_scripts/run_domthreader.sh
@@ -14,8 +14,9 @@ size=${#path}
 FILES=($HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/$1/*)
 out_name=${FILES[$SLURM_ARRAY_TASK_ID-1]}
 out_name=${out_name:$size:-6}
+result_path = $HOME/working/genome3d/domthreader_output/$out_name.presults
 
-if [ -f '$HOME/working/genome3d/domthreader_output/$out_name.presults' ]; then
+if [ -f $result_path ]; then
    echo "skipping $out_name"
 else
   echo "calculating $out_name"
