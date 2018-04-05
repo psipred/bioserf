1d337c77366d7c32a27a02f5046319faf21f1ee4
Daneil Buchan <clone@myself.com>
2018-04-05 15:26:18 +0100

    updating script

--- slurm_helper_scripts/run_domthreader.sh
+++ slurm_helper_scripts/run_domthreader.sh
@@ -9,7 +9,7 @@
 #
 #SBATCH --array=1-1000
 
-path=$HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/
+path=$HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/$1/
 size=${#path}
 FILES=($HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/$1/*)
 out_name=${FILES[$SLURM_ARRAY_TASK_ID-1]}
