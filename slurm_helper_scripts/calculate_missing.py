1c45c5d839a2f94c3f68255e1164b6a53f7663c7
Daneil Buchan <clone@myself.com>
2018-04-05 13:42:19 +0100

    updating script

--- slurm_helper_scripts/calculate_missing.py
+++ slurm_helper_scripts/calculate_missing.py
@@ -3,12 +3,21 @@ import sys
 import re
 import os
 
-#calculate_missing $HOME/working/genome3d/pdbaa_based_models $HOME/working/genome3d/Genome3D.2017-09-05/all_fasta
+# calculate_missing $HOME/working/genome3d/pdbaa_based_models $HOME/working/genome3d/Genome3D.2017-09-05/all_fasta
 
-pdb_str = ".+/(.+?)_\d+_\d+\.pdb"
-pdb_re = re.compile(pdb_str)
 complete_genes = []
-for path in glob.glob(sys.argv[1]+"*_*_*.pdb"):
+# pdb_str = ".+/(.+?)_\d+_\d+\.pdb"
+# pdb_re = re.compile(pdb_str)
+# for path in glob.glob(sys.argv[1]+"*_*_*.pdb"):
+#     # print(path)
+#     m = pdb_re.search(path)
+#     if m:
+#         complete_genes.append(m.group(1))
+
+
+pdb_str = "(.+?)\.pdom\.presults"
+pdb_re = re.compile(pdb_str)
+for path in glob.glob(sys.argv[1]+"*.presults"):
     # print(path)
     m = pdb_re.search(path)
     if m:
