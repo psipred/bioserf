d8bdf0d47430ff6d664aff0fb3c425a29778ab79
Daneil Buchan <clone@myself.com>
2018-04-05 15:03:21 +0100

    updating script

--- slurm_helper_scripts/run_domthreader.sh
+++ slurm_helper_scripts/run_domthreader.sh
@@ -13,7 +13,7 @@ path=$HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/$1/
 size=${#path}
 FILES=($HOME/working/genome3d/Genome3D.2017-09-05/all_fasta/$1/*)
 out_name=${FILES[$SLURM_ARRAY_TASK_ID-1]}
-out_name=${$out_name:$size-6}
+out_name=${$out_name:$size:-6}
 result_path=$HOME/working/genome3d/domthreader_output/$out_name.presults
 
 if [ -f $result_path ]; then
