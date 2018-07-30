1f0bbb1834c3832c8fbf46c7ec5b731c9946dd24
Daniel Buchan <clone@myself.com>
2018-07-30 15:55:50 +0100

    adding file rationalisation script

--- slurm_helper_scripts/move_remaining_seqs.py
+++ slurm_helper_scripts/move_remaining_seqs.py
@@ -15,3 +15,13 @@ for dom_file in glob.glob(results+"*.presults"):
     result_list.append(dom_id)
 
 print(result_list)
+
+targets = '/home/camp/buchand/working/genome3d/Genome3D.2017-09-05/all_fasta/'
+target_list = []
+for target_dir in glob.glob(targets+"*"):
+    for target in glob.glob(target_dir+"/*"):
+        prot_id = target[target.rfind("/")+1:-6]
+        if prot_id in result_list:
+            print(found)
+        else:
+            print("Moving", prot_id)
