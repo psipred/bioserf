5ec4db696fa04e5918e1c8c05eab8c75d443786c
Daniel Buchan <clone@myself.com>
2018-10-01 16:50:05 +0100

    things

--- slurm_helper_scripts/run_modeller.sh
+++ slurm_helper_scripts/run_modeller.sh
@@ -7,14 +7,14 @@
 #SBATCH --ntasks=1
 #SBATCH --array=1-30
 
-FILES=($HOME/modeller_targets/*)
-FILE=${FILES[$SLURM_ARRAY_TASK_ID-1]}
-while read -r line || [[ -n "$line" ]]; do
-    echo "Text read from file: $line"
-done < "$FILE"
-
 cd $HOME/working/genome3d/parse_cath_domth_output/
 export PYTHONPATH=$HOME/Applications/modeller9.19/modlib/:$HOME/Applications/modeller9.19/lib/x86_64-intel8/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Application/modeller9.19/lib/x86_64-intel8/
+
+FILES=($HOME/modeller_targets/*)
+FILE=${FILES[$SLURM_ARRAY_TASK_ID-1]}
+while read -r line || [[ -n "$line" ]]; do
+    $HOME/Applications/modeller9.19/bin/mod9.19 $i.py
+done < "$FILE"
 
 # for i in `find -iname "*.py" | sed -e 's/\.py//'`; do echo $i; $HOME/Applications/modeller9.19/bin/mod9.19 $i.py; done;
