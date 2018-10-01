e17055681fb1153cbaa1978025d7c4551fd02f44
Daniel Buchan <clone@myself.com>
2018-10-01 16:51:04 +0100

    things

--- slurm_helper_scripts/run_modeller.sh
+++ slurm_helper_scripts/run_modeller.sh
@@ -14,7 +14,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Application/modeller9.19/lib/x86_6
 FILES=($HOME/modeller_targets/*)
 FILE=${FILES[$SLURM_ARRAY_TASK_ID-1]}
 while read -r line || [[ -n "$line" ]]; do
-    $HOME/Applications/modeller9.19/bin/mod9.19 $i.py
+    $HOME/Applications/modeller9.19/bin/mod9.19 $line
 done < "$FILE"
 
 # for i in `find -iname "*.py" | sed -e 's/\.py//'`; do echo $i; $HOME/Applications/modeller9.19/bin/mod9.19 $i.py; done;
