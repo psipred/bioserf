f72fddd12a95a4f30e926d19f06284e1a3da7bdf
Daniel Buchan <clone@myself.com>
2018-10-02 12:40:29 +0100

    things

--- slurm_helper_scripts/run_rewrite.sh
+++ slurm_helper_scripts/run_rewrite.sh
@@ -15,8 +15,8 @@ FILES=($HOME/rewrite_targets/*)
 FILE=${FILES[$SLURM_ARRAY_TASK_ID-1]}
 while read -r line || [[ -n "$line" ]]; do
     i=$line
-    tmp=`echo $i | cut -d. -f1`
-    fasta_full=`echo $i | rev | cut -d"_" -f2- | rev`
+    j=`echo $i | cut -d. -f1`
+    fasta_full=`echo $j | rev | cut -d"_" -f2- | rev`
     fasta=`echo $fasta | cut -d"/" -f4`
     #echo $i
     echo $HOME/Applications/bioserf/bin/rewrite_modeller.pl $HOME/working/genome3d/parse_cath_domth_output/ $HOME/$fasta_full.mod_lookups $HOME/$fasta_full.blastaligns $HOME/$fasta_full.pdomaligns $HOME/working/genome3d/Genome3D.2017-09-05/all/$fasta.fasta $HOME/$i $HOME/Applications/bioserf/bin/reformat.pl
