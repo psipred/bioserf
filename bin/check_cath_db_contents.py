4c690c9286dbcf0b1339a98caa2a4665a2f1e1a8
Daniel Buchan <clone@myself.com>
2019-02-07 18:39:06 +0000

    adding script to ensure cathdb is in sync before modelling

--- bin/check_cath_db_contents.py
+++ bin/check_cath_db_contents.py
@@ -1,7 +1,7 @@
 import glob
 import re
 
-cath_id_re = re.compile("\d.{3}\w\d{2}")
+cath_id_re = re.compile("(\d.{3}\w\d{2})")
 for name in glob.glob('*.py'):
     with open(name) as fh:
         contents = fh.readlines()
