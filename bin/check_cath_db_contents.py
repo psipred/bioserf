de940f8db8f183930c364213b439688d7ea92126
Daniel Buchan <clone@myself.com>
2019-02-07 18:31:21 +0000

    adding script to ensure cathdb is in sync before modelling

--- bin/check_cath_db_contents.py
+++ bin/check_cath_db_contents.py
@@ -1,4 +1,9 @@
 import glob
+import re
 
 for name in glob.glob('*.py'):
     print(name)
+    with open(“filename”) as file:
+        line = file.readline()
+        if "knowns=(" in line:
+            print(line)
