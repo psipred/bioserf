60c61e1924b0a71bff0823d5e515de17fe8d607f
Daniel Buchan <clone@myself.com>
2019-02-07 18:53:12 +0000

    adding script to ensure cathdb is in sync before modelling

--- bin/check_cath_db_contents.py
+++ bin/check_cath_db_contents.py
@@ -3,6 +3,7 @@ import re
 import os
 import urllib.request
 
+cath_db = "/data/cath_data/dompdb"
 cath_id_re = re.compile("(\d.{3}\w\d{2})")
 for name in glob.glob('*.py'):
     with open(name) as fh:
@@ -12,5 +13,5 @@ for name in glob.glob('*.py'):
                 result = cath_id_re.search(line)
                 if result:
                     for cath_id in result.groups():
-                        if not os.path.isfile("/data/cath_data/dompdb"+cath_id):
-                            urllib.request.urlretrieve("http://www.cathdb.info/version/v4_2_0/api/rest/id/"+cath_id+".pdb", cath_id)
+                        if not os.path.isfile(cath_db+cath_id):
+                            urllib.request.urlretrieve("http://www.cathdb.info/version/v4_2_0/api/rest/id/"+cath_id+".pdb", cath_db+cath_id)
